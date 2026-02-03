b8c3bb7666fc5ff16e40e03c0c8897102290a406
BouncyBoba <kittycola5432k@gmail.com>
2026-02-03 15:53:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://collection.cloudinary.com/dfmnfe3wa/a378f5fbfbf313138879a932a5c7f249", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dfmnfe3wa/video/upload/v1770150871/lo-fi-music-loop-sentimental-jazzy-love-473154_1_a4rt9p.wav", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
